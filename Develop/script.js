53e82f052c9939feb5cda6980ef0612a9ce02ea1
Sudar Meruva <meruva_s@yaho.com>
2019-12-08 17:19:13 -0500

    Added 4 variables for user input

--- Develop/script.js
+++ Develop/script.js
@@ -21,3 +21,23 @@ function copyToClipboard() {
 generateBtn.addEventListener("click", writePassword);
 
 // BONUS EVENT LISTENER
+
+//main generate password method
+function generatePassword() {
+
+  var pwdlen = prompt("Length (must be between 8 and 128 characters)", 8);
+  console.log(pwdlen);
+  
+  var wantSplchrs = confirm("Do you want to include special characters?");
+  console.log("splchrs "+wantSplchrs);
+  
+  var wantLowerchrs = confirm("Do you want to include Lower characters?");
+  console.log("lower "+wantLowerchrs);
+  
+  var wantUpperchrs = confirm("Do you want to include Upper characters?");
+  console.log("upper "+wantUpperchrs);
+  
+  //" !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
+
+}
